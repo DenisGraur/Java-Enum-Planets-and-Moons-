7116c68c8cedabcf80de3c87be4ce252998164c7
DenisGraur <88393516+DenisGraur@users.noreply.github.com>
2023-02-28 12:05:13 +0000

    adding values to Constants and create a constructor for enum

--- Main.java
+++ Main.java
@@ -5,8 +5,6 @@ public class Main
 	public static void main(String[] args) {
 		
 		Planet Myplanet = Planet.EARTH;
-		Planet firstPlanet = Planet.MERCURY;
-		Planet secondPlanet = Planet.VENUS;
 		IsItHabitable(firstPlanet);
 	}
 	
@@ -14,9 +12,11 @@ public class Main
 	    switch(Myplanet){
 	        case EARTH:
 	            System.out.println("Breath freely");
+                System.out.println("This is planet nr" + Myplanet.number);
 	            break;
 	       default:
 	       System.out.println("Keep your hemlet on !!!");
+           System.out.println("This is planet nr" + Myplanet.number);
 	       break;
 	    }
 	}
@@ -24,13 +24,27 @@ public class Main
 
 
 // create an enum of planets:
-
 enum Planet{
-    MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE, PLUTO
+    // adding values to the constants:
+    MERCURY(1), 
+    VENUS(2), 
+    EARTH(3), 
+    MARS(4), 
+    UPITER(5), 
+    SATURN(6), 
+    URANUS(7), 
+    NEPTUNE(8), 
+    PLUTO(9)
+
+    int number;
+    // create a coonstructor for the enum:
+    Planet(int number){
+        this.number = number;
+    }
 }
 
-// create an enum oof moons:
 
+// create an enum oof moons:
 enum Moon {
     LUNA, PHOBOS, TITAN, EUROPA
 }
